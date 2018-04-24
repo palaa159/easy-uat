68d44b7c7a970f71754801eedb8cdd9b97564b41
Por <nattida@5lab.co>
2018-04-24 17:40:22 +0700

    _add term-condition page

--- components/text/PageHeading.vue
+++ components/text/PageHeading.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="_lh-100pct _cl-light-purple _tal-ct _fs-4 _fw-600 _pdv-32px" v-html="text"/>  
+  <h1 class="_lh-100pct _cl-light-purple _tal-ct _fs-4 _fw-600 _pdv-32px _w-100pct" v-html="text"/>  
 </template>
 
 <script>
