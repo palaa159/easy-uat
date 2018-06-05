4ca02073e57b10addbb5afba4245299715ccd983
Apon Palanuwech <palaa159@newschool.edu>
2018-06-06 00:26:07 +0700

    - save

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -64,8 +64,8 @@ export default {
   position: fixed;
   z-index: 1;
   transition: 0.5s;
-  box-shadow: 0px -10px 20px 10px rgba(0, 0, 0, 0.1);
-  border-bottom: 1px solid rgba(255, 255, 255, 0.35);
+  box-shadow: 0px 1px 20px 10px rgba(0, 0, 0, 0.1);
+  // border-bottom: 1px solid rgba(0, 0, 0, 0.35);
   top: -200px;
   animation: menu-slide-down 0.5s linear;
   animation-fill-mode: forwards;
