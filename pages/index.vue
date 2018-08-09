2733369e43863d67d77a9a088625de9c7551fba5
Five Lab <5lab@Fives-MacBook-Pro.local>
2018-08-09 19:45:35 +0700

    Start iggu

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-12">
-          <h3>IGshopping.co</h3>
+          <h3>IGGU.ME</h3>
         </div>
       </div>
     </div>
