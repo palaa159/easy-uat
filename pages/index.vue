5f78a8640b3ce8b37a08fc1d2ad9623e3bdf2cc7
Your Name <you@example.com>
2018-08-27 10:51:08 +0700

    update template

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,97 @@
 <template>
-  <div class="_w-100pct">
-    <div class="container-fluid">
+<!-- open div -->
+<div class="_w-100pct">
+    <div class="container _bgcl-neutral-100">
+    <div class="container-fluid _bgcl-primary-300">
       <div class="row">
       <div class="col-12 myHeader">
-        Easy UAT
+      <h1 class="text">Easy UAT</h1>  
       </div>
     </div>
   </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-12 _pd-16px btn-create">
-          <button type="button" class="bio-button"><nuxt-link to="/home">CREATE</nuxt-link></button>
-        </div>
-        <div class="col-4 _pd-16px">
-          <div class="box">
-            A
-          </div>
-        </div>
-         <div class="col-4 _pd-16px">
-          <div class="box">
-            B
-          </div>
-        </div>
-         <div class="col-4 _pd-16px">
-          <div class="box">
-            C
-          </div>
-        </div>
-         <div class="col-4 _pd-16px">
-          <div class="box">
-            D
-          </div>
-        </div>
-         <div class="col-4 _pd-16px">
-          <div class="box">
-            E
-          </div>
-        </div>
-         <div class="col-4 _pd-16px">
-          <div class="box">
-            F
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <br>
+<div class="bio-button u-rise _dp-il bn "><nuxt-link to="/addpage">Create Project</nuxt-link></div>
+<br>
+<br>
+    <!-- open div container -->
+<div class="container _bgcl-neutral-300 _pd-16px">
+    <!-- open div row -->
+    <div class="row">
+        <!-- open div col -->
+    <div class="col-4 _pd-16px">
+        <div class="bio-card h-100">
+             <img class="bio-card-cover" src="https://placehold.it/150x80?text=IMAGE" alt="image">
+                <div class="bio-card-title"> <strong>Project 1</strong>
+                        </div>
+                        <div class="bio-card-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                             Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</div>
+                    
+             </div>
+     </div>
+     <!-- colse div col -->
+     <!-- open div col -->
+     <div class="col-4 _pd-16px">
+        <div class="bio-card h-100">
+             <img class="bio-card-cover" src="https://placehold.it/150x80?text=IMAGE" alt="image">
+                <div class="bio-card-title"> <strong>Project 1</strong>
+                        </div>
+                         <div class="bio-card-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                             Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</div>
+                   
+             </div>
+     </div>
+     <!-- colse div col -->
+     <!-- open div col -->
+     <div class="col-4 _pd-16px">
+        <div class="bio-card h-100">
+             <img class="bio-card-cover" src="https://placehold.it/150x80?text=IMAGE" alt="image">
+                <div class="bio-card-title"> <strong>Project 1</strong>
+                        </div>
+                         <div class="bio-card-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                             Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</div>
+                    
+             </div>
+     </div>
+     <!-- colse div col -->
+        <!-- open div col -->
+     <div class="col-4 _pd-16px">
+        <div class="bio-card h-100">
+             <img class="bio-card-cover" src="https://placehold.it/150x80?text=IMAGE" alt="image">
+                <div class="bio-card-title"> <strong>Project 1</strong>
+                        </div>
+                         <div class="bio-card-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                             Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</div>
+                    
+             </div>
+     </div>
+      <!-- colse div col -->
+        <!-- open div col -->
+     <div class="col-4 _pd-16px">
+        <div class="bio-card h-100">
+             <img class="bio-card-cover" src="https://placehold.it/150x80?text=IMAGE" alt="image">
+                <div class="bio-card-title"> <strong>Project 1</strong>
+                        </div>
+                         <div class="bio-card-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                             Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</div>
+                    
+             </div>
+     </div>
+     <!-- colse div col -->
+      <!-- open div col -->
+     <div class="col-4 _pd-16px">
+        <div class="bio-card h-100">
+             <img class="bio-card-cover" src="https://placehold.it/150x80?text=IMAGE" alt="image">
+                <div class="bio-card-title"> <strong>Project 1</strong>
+                        </div>
+                         <div class="bio-card-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                             Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.</div>
+                    
+             </div>
+     </div><!-- colse div col -->
+     
+</div><!-- close div row -->
+
+</div><!-- close div container -->
+</div>
+</div><!-- close div -->
 </template>
-<style lang="scss" scoped>
-.myHeader {
-  
-  height: 100px;
-  color: white;
-  font-size: 50px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: black;
-}
-.btn-create {
-  display:flex;
-  justify-content:flex-end;
-  align-items:center; 
-}
-</style>
