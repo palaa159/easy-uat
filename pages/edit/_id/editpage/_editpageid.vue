944d18f60743f0ccfb0581f1cf63500ec845ac37
KandaOil <kanda_27408@hotmail.com>
2018-09-24 10:46:42 +0700

    link index

--- pages/edit/_id/editpage/_editpageid.vue
+++ pages/edit/_id/editpage/_editpageid.vue
@@ -10,7 +10,11 @@
             <div class="container-fluproject_id _bgcl-primary-300">
                 <div class="row">
                     <div class="col-12 myHeader">
-                        <h1 class="text">Easy UAT</h1>  
+                        <h1 class="text">
+                            <nuxt-link to="/">
+                                Easy UAT
+                            </nuxt-link> 
+                        </h1>  
                     </div>
                 </div>
             </div>
